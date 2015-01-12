e19184c68c3f3ab3d8ecf2a1be7adce24e49d66f
Konrad Baron <konradbaron@gmail.com>
2015-01-11 21:45:05 -0700

    Update MeetupGroups.class.php
    Added additional available geolocation parameters to getGroups method. Specifically; country, city, state, zip, lat, lon, radius. This will allow the method to return groups based on a location request. For example if you wanted all groups in Denver (city), Colorado (state).

--- MeetupGroups.class.php
+++ MeetupGroups.class.php
@@ -11,15 +11,15 @@ class MeetupGroups extends MeetupApiRequest {
     /**
      * Returns all groups matching the given criteria
      *
-     * Required paramters:
-     * 'campaign_id' | 'domain' | 'group_id' | 'group_urlname' | 'member_id' | 'organizer_id' | 'sponsor_id'
+     * Required parameters:
+     * 'campaign_id' | 'domain' | 'group_id' | 'group_urlname' | 'member_id' | 'organizer_id' | 'sponsor_id' | 'topic' | 'country' | 'city' | 'state' | 'zip' | 'lat' | 'lon' | 'radius'
      *
      * @link http://www.meetup.com/meetup_api/docs/2/groups
      * @param Array $Parameters
      * @return Array
      */
     public function getGroups( $Parameters ) {
-        $required_params = array( 'campaign_id', 'domain', 'group_id', 'group_urlname', 'member_id', 'organizer_id', 'sponsor_id', 'topic');
+        $required_params = array( 'campaign_id', 'domain', 'group_id', 'group_urlname', 'member_id', 'organizer_id', 'sponsor_id', 'topic', 'country', 'city', 'state', 'zip', 'lat', 'lon', 'radius');
         $url = $this->buildUrl( MEETUP_ENDPOINT_GROUPS, $Parameters, $required_params );
         $response =  $this->get( $url )->getResponse();
         return $response['results'];
@@ -28,7 +28,7 @@ class MeetupGroups extends MeetupApiRequest {
     /**
      * Returns all comments associated with the given group
      *
-     * Required paramters:
+     * Required parameters:
      * 'group_id' | 'group_urlname' | 'topic,groupnum'
      *
      * @link http://www.meetup.com/meetup_api/docs/comments
